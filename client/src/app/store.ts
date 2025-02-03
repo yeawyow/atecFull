65c6bcb4f2fb95e3d4e8c3e97ef4f0b40642a26b
yeawyow <yeawyow@gmail.com>
2025-02-03 10:49:16 +0700

    เพิ่ม เมนู

--- client/src/app/store.ts
+++ client/src/app/store.ts
@@ -5,6 +5,6 @@ export const store = configureStore({
   reducer: { auth: authReducer },
 });
 
-// Types สำหรับ RootState และ Dispatch
+
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
