3ffd8834f3ba54f2e43cee4ca6c788329c7ac2c3
yeawyow <yeawyow@gmail.com>
2025-02-05 14:26:16 +0700

    deploy with github

--- client/src/config.ts
+++ client/src/config.ts
@@ -2,7 +2,7 @@ const config = {
   API_URL: 'http://localhost:3000', // URL ของ backend
   API_LOGIN: '/auth/login',
   API_VERIFY_TOKEN:'/auth/check_token',
-  APP_TITLE: 'ระบบจัดการเรียนการสอน',
+  APP_TITLE: 'ระบบการจัดการเรียนรู้ anywhere anytime',
 };
 
 export default config;
