2b07945cf3f50c12f559956a0ced64d88ca60c2d
yeawyow <yeawyow@gmail.com>
2025-02-05 13:56:16 +0700

    config.vite

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        charset: 'utf-8'  // ✅ บังคับใช้ UTF-8 ในไฟล์ output
+      }
+    }
+  }
 })
