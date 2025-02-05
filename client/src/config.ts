71bf10029c7c6ffca99303442fcc494ae13a3098
yeawyow <yeawyow@gmail.com>
2025-02-05 13:29:14 +0700

    llll

--- client/src/config.ts
+++ client/src/config.ts
@@ -2,7 +2,7 @@ const config = {
   API_URL: 'http://localhost:3000', // URL ของ backend
   API_LOGIN: '/auth/login',
   API_VERIFY_TOKEN:'/auth/check_token',
-  APP_TITLE: 'โปรแกรมบริหารจัดการการเรียนdddddddeee',
+  APP_TITLE: 'fffff',
 };
 
 export default config;
